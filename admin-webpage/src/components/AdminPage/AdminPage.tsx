e4ae850f4e57791ad47d280114f1980dd876bc8e
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-11-17 19:38:04 +0200

    1 created default react ts app

---
+++ admin-webpage/src/components/AdminPage/AdminPage.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import { AdminPageWrapper } from './AdminPage.styled';
+
+interface AdminPageProps {}
+
+const AdminPage: FC<AdminPageProps> = () => (
+   <AdminPageWrapper>
+      AdminPage Component
+   </AdminPageWrapper>
+);
+
+export default AdminPage;
