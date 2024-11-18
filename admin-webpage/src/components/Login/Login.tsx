356f0f1313b87aeb1f35411685679a77aeebb6e5
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-11-18 08:31:26 +0200

    3 done logic of handle login form

--- admin-webpage/src/components/Login/Login.tsx
+++ admin-webpage/src/components/Login/Login.tsx
@@ -13,6 +13,9 @@ const Login: FC = () => {
    const [_username, setUsername] = useState<string>("none");
    const [_password, setPassword] = useState<string>("none");
    const [loginModel, setLoginModel] = useState<ILoginModel | null>(null);
+   const [isLogin, setIsLogin] = useState<boolean>(false);
+   const [statusCode, setStatusCode] = useState<number>(0);
+   const [loginMessage, setLoginMessage] = useState<string>('');
 
    const handleName = (e: React.FormEvent<HTMLElement>) => {
       setUsername((e.currentTarget as HTMLInputElement).value);
@@ -30,32 +33,53 @@ const Login: FC = () => {
       }  
    }, [_username, _password]);
    
-
-   const handleSubmit = (e: React.FormEvent<HTMLElement>) => {
+   const handleLogin = async (e: React.FormEvent<HTMLElement>) => {
       e.preventDefault();
-      console.log(loginModel);
+
       if(loginModel != null){
-         fetchToken(loginModel);
-         setUsername('none');
-         setPassword('none');
+         setStatusCode(await fetchToken(loginModel));
       }
-      console.log(_username);
+   }
+
+   const handleLoginMessage = () => {
+      if (statusCode == 200){
+         setIsLogin(true);
+         setLoginMessage('Authorized');
+      }
+      else if (statusCode == 401){
+         setIsLogin(false);
+         setLoginMessage('Unauthorized');
+      }
+      else{
+         setIsLogin(false);
+         setLoginMessage('');
+      }
+   }
+
+   useEffect(() => {handleLoginMessage();}, [statusCode]);
+
+   const handleLogout = () => {
+      localStorage.removeItem('token')
+      setStatusCode(0);
+      setUsername('none');
+      setPassword('none');
    }
 
    return (
       <LoginWrapper>
          <p>Authorization</p>
          <form>
-           <label>
-              User Name:
-              <input type="text" placeholder={_username} onBlur={handleName}/>
-           </label>
-           <label>
-              Password:
-              <input type="text" placeholder={_password} onBlur={handlePassword}/>
-           </label>
-           <button type='button' onClick={handleSubmit}>Login</button>
-           <button type='button'>Logout</button>
+            <label>
+               User Name:
+               <input type="text" placeholder={_username} onBlur={handleName} disabled = {isLogin}/>
+            </label>
+            <label>
+               Password:
+               <input type="text" placeholder={_password} onBlur={handlePassword} disabled = {isLogin}/>
+            </label>
+            <button type='button' onClick={handleLogin} disabled = {isLogin}>Login</button>
+            <button type='button' onClick={handleLogout}>Logout</button>
+            <p>{loginMessage}</p>
          </form>
       </LoginWrapper>
      );
