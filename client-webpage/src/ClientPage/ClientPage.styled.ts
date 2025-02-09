7b4f1c3fa1575d2a1f697968f8d35ad4a56be940
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2025-02-09 09:27:58 +0200

    2 created structure of client page react app

---
+++ client-webpage/src/ClientPage/ClientPage.styled.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const ClientPageWrapper = styled.main`
+    background-color:rgb(90, 100, 121);
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    /* font-size: calc(10px + 2vmin); */
+    text-align: center;
+    color: white;
+`
---
+++ client-webpage/src/ClientPage/ClientPage.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+import 'the-new-css-reset';
+import { ClientPageWrapper as _main} from "./ClientPage.styled";
+
+const ClientPage: FC = () => {
+    return(
+    <_main>
+        <p>client page</p>
+    </_main>)
+}
+
+export default ClientPage;
